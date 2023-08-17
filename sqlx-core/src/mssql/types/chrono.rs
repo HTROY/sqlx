0040b437b97512ebee9bafda2a42977ceb361a92
HTROY <htroyvip@gmail.com>
2023-08-17 17:20:18 +0800

    fix compile

--- sqlx-core/src/mssql/types/chrono.rs
+++ sqlx-core/src/mssql/types/chrono.rs
@@ -97,14 +97,14 @@ impl<'r> Decode<'r, Mssql> for NaiveDateTime {
                 let days = LittleEndian::read_u32(&value.as_bytes()?[0..3]);
                 let scale = value.type_info.0.scale;
                 let seconds = match scale {
-                    exp @ 0 | 1 | 2 => {
-                        LittleEndian::read_u32(&value.as_bytes()?[3..6]) / 10u32.pow(exp.into())
+                    0 | 1 | 2 => {
+                        LittleEndian::read_u32(&value.as_bytes()?[3..6]) / 10u32.pow(scale.into())
                     }
-                    exp @ 3 | 4 => {
-                        LittleEndian::read_u32(&value.as_bytes()?[3..7]) / 10u32.pow(exp.into())
+                    3 | 4 => {
+                        LittleEndian::read_u32(&value.as_bytes()?[3..7]) / 10u32.pow(scale.into())
                     }
-                    exp @ 5 | 6 | 7 => {
-                        LittleEndian::read_u32(&value.as_bytes()?[3..8]) / 10u32.pow(exp.into())
+                    5 | 6 | 7 => {
+                        LittleEndian::read_u32(&value.as_bytes()?[3..8]) / 10u32.pow(scale.into())
                     }
                     _ => unreachable!(),
                 };
