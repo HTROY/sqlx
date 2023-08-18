ae7a36c28f9fe967d7cf80113aee52a662b00602
HTROY <htroyvip@gmail.com>
2023-08-18 15:12:06 +0800

    fix problem

--- sqlx-core/src/mssql/types/chrono.rs
+++ sqlx-core/src/mssql/types/chrono.rs
@@ -1,4 +1,4 @@
-use byteorder::{ByteOrder, LittleEndian};
+use byteorder::{BigEndian, ByteOrder, LittleEndian};
 use chrono::{DateTime, Duration, Local, NaiveDate, NaiveDateTime, NaiveTime, TimeZone, Utc};
 
 use crate::decode::Decode;
@@ -22,7 +22,14 @@ impl Type<Mssql> for NaiveDate {
 
 impl Type<Mssql> for NaiveDateTime {
     fn type_info() -> MssqlTypeInfo {
-        MssqlTypeInfo(TypeInfo::new(DataType::DateTime, 8))
+        MssqlTypeInfo(TypeInfo::new(DataType::DateTimeN, 8))
+    }
+
+    fn compatible(ty: &<Mssql as crate::database::Database>::TypeInfo) -> bool {
+        matches!(
+            ty.0.ty,
+            DataType::DateTime | DataType::DateTimeN | DataType::DateTime2N
+        )
     }
 }
 /*
@@ -69,8 +76,8 @@ impl<'r> Decode<'r, Mssql> for NaiveDate {
 
 impl Encode<'_, Mssql> for NaiveDateTime {
     fn encode_by_ref(&self, buf: &mut Vec<u8>) -> IsNull {
-        let days_duration = self.date() - NaiveDate::from_ymd(1900, 1, 1);
-        let ms_duration = self.time() - NaiveTime::from_hms(0, 0, 0);
+        let days_duration = self.date() - NaiveDate::from_ymd_opt(1900, 1, 1).unwrap();
+        let ms_duration = self.time() - NaiveTime::from_hms_opt(0, 0, 0).unwrap();
         let days = days_duration.num_days() as i32;
         let ms = ms_duration.num_milliseconds() as u32 * 3 / 10;
 
@@ -88,29 +95,37 @@ impl<'r> Decode<'r, Mssql> for NaiveDateTime {
                 let third_seconds = LittleEndian::read_u32(&value.as_bytes()?[4..8]);
                 let ms = third_seconds / 3 * 10;
 
-                let time = NaiveTime::from_hms(0, 0, 0) + Duration::milliseconds(ms.into());
-                let date = NaiveDate::from_ymd(1900, 1, 1) + Duration::days(days.into());
+                let time =
+                    NaiveTime::from_hms_opt(0, 0, 0).unwrap() + Duration::milliseconds(ms.into());
+                let date =
+                    NaiveDate::from_ymd_opt(1900, 1, 1).unwrap() + Duration::days(days.into());
 
                 Ok(date.and_time(time))
             }
             DataType::DateTime2N => {
-                let days = LittleEndian::read_u32(&value.as_bytes()?[0..3]);
+                let days = LittleEndian::read_u24(&value.as_bytes()?[5..8]);
                 let scale = value.type_info.0.scale;
                 let seconds = match scale {
                     0 | 1 | 2 => {
-                        LittleEndian::read_u32(&value.as_bytes()?[3..6]) / 10u32.pow(scale.into())
+                        LittleEndian::read_u24(&value.as_bytes()?[0..3]) as u64
                     }
                     3 | 4 => {
-                        LittleEndian::read_u32(&value.as_bytes()?[3..7]) / 10u32.pow(scale.into())
+                        LittleEndian::read_u32(&value.as_bytes()?[0..4]) as u64
                     }
                     5 | 6 | 7 => {
-                        LittleEndian::read_u32(&value.as_bytes()?[3..8]) / 10u32.pow(scale.into())
+                        let mut slice = [0u8; 6];
+                        slice.copy_from_slice(&value.as_bytes()?[0..6]);
+                        slice[5] = 0;
+                        LittleEndian::read_u48(&slice)
                     }
                     _ => unreachable!(),
                 };
 
-                let time = NaiveTime::from_hms(0, 0, 0) + Duration::seconds(seconds.into());
-                let date = NaiveDate::from_ymd(1, 1, 1) + Duration::days(days.into());
+                let nano_seconds = seconds * 10u64.pow(9 - scale as u32);
+                let (seconds, nano_seconds) = (nano_seconds / 1_000_000_000, nano_seconds % 1_000_000_000);
+                let time =
+                    NaiveTime::from_hms_nano_opt(0, 0, 0, nano_seconds as u32).unwrap() + Duration::seconds(seconds as i64);
+                let date = NaiveDate::from_ymd_opt(1, 1, 1).unwrap() + Duration::days(days.into());
                 Ok(date.and_time(time))
             }
             DataType::DateTimeOffsetN => {
@@ -118,8 +133,10 @@ impl<'r> Decode<'r, Mssql> for NaiveDateTime {
                 let third_seconds = LittleEndian::read_u32(&value.as_bytes()?[4..8]);
                 let ms = third_seconds / 3 * 10;
 
-                let time = NaiveTime::from_hms(0, 0, 0) + Duration::milliseconds(ms.into());
-                let date = NaiveDate::from_ymd(1900, 1, 1) + Duration::days(days.into());
+                let time =
+                    NaiveTime::from_hms_opt(0, 0, 0).unwrap() + Duration::milliseconds(ms.into());
+                let date =
+                    NaiveDate::from_ymd_opt(1900, 1, 1).unwrap() + Duration::days(days.into());
 
                 Ok(date.and_time(time))
             }
